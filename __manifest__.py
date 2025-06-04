f6f93de77b92025b1ed76d33c9a93249e7aac6b5
Vladimir Kanchev <95029070+vladikanchev@users.noreply.github.com>
2025-06-04 14:04:41 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -3,14 +3,14 @@
     "version": "1.1",
     "category": "Human Resources",
     "summary": "Limits Kiosk Mode Check-in and Check-out by time with Bulgarian voice feedback",
-    "author": "Custom",
+    "author": "Vladi-teo",
     "depends": ["hr_attendance"],
     "data": [
         "views/kiosk_assets.xml"
     ],
     "assets": {
         "web.assets_frontend": [
-            "hr_attendance_kiosk_limit/static/src/js/kiosk_voice.js"
+            "hr_attendance_kiosk_limit_bg/static/src/js/kiosk_voice.js"
         ]
     },
     "installable": True,
